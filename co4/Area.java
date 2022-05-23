8c8a6a106a8d977e2154f9811e9d6c17cc12e0e6
pranavrkvtk <vtkrkpranav@gmail.com>
2022-05-23 09:46:35 +0530

    First commit

---
+++ co4/Area.java
@@ -0,0 +1,17 @@
+import Graphics.*;
+
+public class Area{
+    public static void main(String[] args) {
+        Figure s = new Figure();
+     Scanner s = new scanner(system.in);
+
+        System.out.println("Enter the length");
+        int n =s.nextInt();
+        System.out.println("Enter the breadth");
+        int b=s.next
+        
+        System.out.println("Area of Triangle:"+s.triangleArea(7.0,3.0));
+        System.out.println("Area of Square:"+s.squareArea(5));
+        System.out.println("Area of Circle:"+s.circleArea(6.0));
+    }
+}
